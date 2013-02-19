2bb537e962e981cf54ddda847b58d1004dd99986
Ghaith Tarawneh <gtarawneh@gmail.com>
2013-02-19 16:29:40 +0000

    base commit

---
+++ create_topology.pl
@@ -0,0 +1,76 @@
+$w=4;
+$h=4;
+
+for ($y=0; $y<$h; $y++)
+{
+	for ($x=0; $x<$w-1; $x++)
+	{
+		$r1 = getrouterid($x,$y);
+		$r2 = getrouterid($x+1,$y);
+		#print "router $r1 east -> router $r2 west\n";
+		print "# horizontal channels between routers $r1 and $r2\n";
+		print "rr,$r1,$r2,2,3\n";
+		print "rr,$r2,$r1,3,2\n";
+		#print "router $r1 east <- router $r2 west\n";
+	}
+}
+
+for ($y=0; $y<$h-1; $y++)
+{
+	for ($x=0; $x<$w; $x++)
+	{
+		$r1 = getrouterid($x,$y);
+		$r2 = getrouterid($x,$y+1);
+		print "# vertical channels between routers $r1 and $r2\n";
+		print "rr,$r1,$r2,1,0\n";
+		print "rr,$r2,$r1,0,1\n";
+		#print "router $r1 south -> router $r2 north\n";
+		#print "router $r1 south <- router $r2 north\n";
+	}
+}
+
+for ($x=0; $x<$w; $x++)
+{
+	$r1 = getrouterid($x,0);
+	#print "router $r1 north -> terminator\n";
+	print "# north terminator\n";
+	print "tx,$r1,0\n";
+	print "rx,$r1,0\n";
+	$r1 = getrouterid($x,$h-1);
+	#print "router $r1 south -> terminator\n";
+	print "# south terminator\n";
+	print "tx,$r1,1\n";
+	print "rx,$r1,1\n";
+}
+
+for ($y=0; $y<$h; $y++)
+{
+	$r1 = getrouterid(0,$y);
+	#print "router $r1 west -> terminator\n";
+	print "# west terminator\n";
+	print "tx,$r1,3\n";
+	print "rx,$r1,3\n";
+	$r1 = getrouterid($w-1,$y);
+	#print "router $r1 east -> terminator\n";
+	print "# east terminator\n";
+	print "tx,$r1,2\n";
+	print "rx,$r1,2\n";	
+}
+
+for ($i=0; $i<$h * $w; $i++)
+{
+	#print "router $i local -> sink\n";
+	#print "router $i local <- source\n";
+
+	print "#source\n";
+	print "sr,$i,$i,4\n";
+	
+	print "#sink\n";
+	print "rs,$i,4,$i\n";
+
+}
+
+sub getrouterid
+{
+	4 * $_[1] + $_[0];
+}
---
+++ sink.v
@@ -0,0 +1,85 @@
+
+
+module serial_sink (clk, reset, channel_busy, serial_in, througput);
+
+	input clk, reset, serial_in;
+	
+	output wire [25:0]througput;
+	output channel_busy;
+	
+	wire [`SIZE-1:0] data;
+	
+	sink s1 (.clk(clk), .reset(reset), .data(data), .req(req), .busy(busy), .througput(througput));
+
+	rx rx1 (.clk(clk), .reset(reset), .valid(req), .channel_busy(channel_busy), .item_read(!busy), .serial_in(serial_in), .parallel_out(data) );
+
+endmodule
+
+module sink (clk, reset, data, req, busy, througput);
+/*
+	input clk, reset, req;
+	
+	output busy; assign busy = 0;
+	
+	input [`SIZE-1:0] data;
+	
+	reg [`SIZE-1:0] register;
+	
+	always @(posedge clk or posedge reset) begin
+	
+		if (reset) begin
+			register <= 0;
+		end else begin
+			if (req) register <= data;
+		end			
+	
+	end
+*/
+	input clk, reset, req;
+	
+	output busy;
+
+	output reg [25:0]througput;
+	
+	reg [25:0]running_througput;
+	
+	reg [9:0]sampler;
+	
+	input [`SIZE-1:0] data;
+	
+	reg [`SIZE-1:0] register;
+	
+	reg busy;
+	
+	//reg [7:0] rand;
+	
+	always @(posedge clk or posedge reset) begin
+	
+		if (reset) begin
+			register <= 0;
+			busy <= 1;
+			sampler<=0;
+			//rand <= 0;
+		end else begin
+			//rand <= $random;
+			if (&(sampler)) begin
+				througput<=running_througput;
+				sampler<=0;
+				running_througput<=0;
+			end
+			else begin
+				sampler<=sampler+1;
+				running_througput <= running_througput + (req);
+			end
+			
+			if (req) begin
+				register <= data;
+				$display ("sinked flit  : ", data);
+			end
+			//busy <= (rand > 50);
+			busy <= 0;
+		end			
+	
+	end
+
+endmodule
