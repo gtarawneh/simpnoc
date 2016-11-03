1652f9204f9bec2ce78ed9ab01feebd68eec33ca
Ghaith Tarawneh <gtarawneh@gmail.com>
2016-11-03 19:07:13 +0000

    Implement few stub methods in create_noc.py

--- create_noc.py
+++ create_noc.py
@@ -5,34 +5,94 @@ from textwrap import dedent
 def main():
 	print(str1)
 
-str1 = """
-	connector #(
-		.SIZE(SIZE),
-		.TX_PORT_COUNT(PORT_COUNT),
-		.RX_PORT_COUNT(PORT_COUNT),
-		.TX_PORT(0),
-		.RX_PORT(1)
-	) con2 (
-		.tx_req(tx_req[1]),
-		.tx_ack(tx_ack[1]),
-		.tx_data(tx_data[1]),
-		.rx_req(rx_req[2]),
-		.rx_ack(rx_ack[2]),
-		.rx_data(rx_data[2])
-	);
-"""
-
 def insertSource(id):
-	pass
+	code = """
+		// source %ID
+
+		source2 #(
+			.ID(%ID),
+			.DESTINATION(100 + %ID),
+			.MAX_FLITS(1),
+			.SIZE(SIZE),
+			.PAYLOAD(10 + %ID)
+		) src1 (
+			.clk(clk),
+			.reset(reset),
+			.req(src_req[%ID]),
+			.ack(src_ack[%ID]),
+			.data(src_data[%ID])
+		);
+	"""
+	return insertCode(code, {"%ID": str(id)})
 
 def insertSink(id):
-	pass
+	code ="""
+		// sink %ID
+
+		sink2 #(
+			.ID(%ID)
+		) snk1 (
+			.clk(clk),
+			.reset(reset),
+			.req(snk_req[%ID]),
+			.ack(snk_ack[%ID]),
+			.data(snk_data[%ID])
+		);
+	"""
+	return insertCode(code, {"%ID": str(id)})
 
 def insertConnectionRR(router1, router2, port1, port2):
-	pass
+	code = """
+		// connection: router %R1 (port %P1) -> router %R2 (port %P2)
 
-def insertConnectionSR(source, router):
-	pass
+		connector #(
+			.SIZE(SIZE),
+			.TX_PORT_COUNT(PORT_COUNT),
+			.RX_PORT_COUNT(PORT_COUNT),
+			.TX_PORT(%P1),
+			.RX_PORT(%P2)
+		) con2 (
+			.tx_req(tx_req[%R1]),
+			.tx_ack(tx_ack[%R1]),
+			.tx_data(tx_data[%R1]),
+			.rx_req(rx_req[%R2]),
+			.rx_ack(rx_ack[%R2]),
+			.rx_data(rx_data[%R2])
+		);
+	"""
+	reps = {
+		"%R1" : str(router1),
+		"%R2" : str(router2),
+		"%P1" : str(port1),
+		"%P2" : str(port2),
+	}
+	return insertCode(code, reps)
+
+def insertConnectionSR(source, router, port):
+	code = """
+		// connection: source %SOURCE -> router %ROUTER (port %PORT)
+
+		connector #(
+			.SIZE(SIZE),
+			.TX_PORT_COUNT(1),
+			.RX_PORT_COUNT(PORT_COUNT),
+			.TX_PORT(0),
+			.RX_PORT(%PORT)
+		) con1 (
+			.tx_req(src_req[%SOURCE]),
+			.tx_ack(src_ack[%SOURCE]),
+			.tx_data(src_data[%SOURCE]),
+			.rx_req(rx_req[0]),
+			.rx_ack(rx_ack[0]),
+			.rx_data(rx_data[0])
+		);
+	"""
+	reps = {
+		"%SOURCE" : str(source),
+		"%ROUTER" : str(router),
+		"%PORT" : str(port),
+	}
+	return insertCode(code, reps)
 
 def insertConnectionRS(router, sink):
 	pass
@@ -69,7 +129,6 @@ def insertRouter(id):
 
 		assign table_data[%ID] = 0; // dummy routing table
 	"""
-	# print(dedent(code).replace("%ID", str(id)))
 	return insertCode(code, {"%ID": str(id)})
 
 def insertCode(code, replaceDict):
@@ -78,9 +137,10 @@ def insertCode(code, replaceDict):
 		code = code.replace(identifier, replaceDict[identifier])
 	return code
 
-
 def main():
 	with open("output/gen_routers_test.v", "w") as fid:
-		for i in range(1):
-			fid.write(insertRouter(i))
+		fid.write(insertRouter(0))
+		fid.write(insertConnectionRR(1, 3, 0, 1))
+		fid.write(insertSource(5))
+		fid.write(insertConnectionSR(2, 3, 5))
 main()
