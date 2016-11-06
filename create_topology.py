1948f2fde05c0acba0a103bc1902f235cc2f4e24
Ghaith Tarawneh <gtarawneh@gmail.com>
2016-11-06 15:13:40 +0000

    Add routing tables

--- create_topology.py
+++ create_topology.py
@@ -3,7 +3,7 @@
 import json
 
 def getRouterID(x, y):
-	return "router_%d%d" % (y, x)
+	return "router_%d%d" % (x, y)
 
 def getSinkID(x, y):
 	return "sink_local_%d%d" % (x, y)
@@ -21,18 +21,22 @@ def getTerminatorID(ttype, location, i):
 	else:
 		raise Exception("unknown terminator type")
 
+def getSerializedCoords(width, height):
+	# returns a list of (x, y) tuples arranged in increasing
+	# order of x then y
+	return [(x, y) for y in range(height) for x in range(width)]
+
 def main():
 	noc = {}
 	# noc parameters
 	width = 2
 	height = 2
 	# add routers
-	routers = [(x,y) for x in range(width) for y in range(height)]
-	for x, y in routers:
+	for x, y in getSerializedCoords(width, height):
 		isBorderLeft = x == 0
 		isBorderRight = x == width-1
-		isBorderTop = y == 0
-		isBorderBottom = y == height-1
+		isBorderTop = y == height-1
+		isBorderBottom = y == 0
 		leftTerm = {"destination": getTerminatorID("rx", "left", y), "port": "0"}
 		rightTerm = {"destination": getTerminatorID("rx", "right", y), "port": "0"}
 		topTerm = {"destination": getTerminatorID("rx", "top", x), "port": "0"}
@@ -40,8 +44,8 @@ def main():
 		localTerm = {"destination": getSinkID(x, y), "port": "0"}
 		rightRouter = {"destination": getRouterID(x+1, y), "port": "left"}
 		leftRouter = {"destination": getRouterID(x-1, y), "port": "right"}
-		topRouter = {"destination": getRouterID(x, y-1), "port": "bottom"}
-		bottomRouter = {"destination": getRouterID(x, y+1), "port": "top"}
+		topRouter = {"destination": getRouterID(x, y+1), "port": "bottom"}
+		bottomRouter = {"destination": getRouterID(x, y-1), "port": "top"}
 		cons = {
 			"right": rightTerm if isBorderRight else rightRouter,
 			"left": leftTerm if isBorderLeft else leftRouter,
@@ -58,7 +62,7 @@ def main():
 			}
 		}
 	# add sinks
-	localSinks = [getSinkID(x, y) for x in range(width) for y in range(height)]
+	localSinks = [getSinkID(x, y) for x, y in getSerializedCoords(width, height)]
 	topSinks = [getTerminatorID("rx", "top", x) for x in range(width)]
 	bottomSinks = [getTerminatorID("rx", "bottom", x) for x in range(width)]
 	leftSinks = [getTerminatorID("rx", "left", x) for x in range(height)]
@@ -70,8 +74,7 @@ def main():
 			"address": str(ind)
 		}
 	# add local sources
-	sources = [(x,y) for x in range(width) for y in range(height)]
-	for x, y, in sources:
+	for x, y, in getSerializedCoords(width, height):
 		cons = {
 			"0": {
 				"destination": getRouterID(x, y),
@@ -87,6 +90,26 @@ def main():
 	for y in range(height):
 		noc[getTerminatorID("tx", "left", y)] = {"class": "source", "flits": 0}
 		noc[getTerminatorID("tx", "right", y)] = {"class": "source", "flits": 0}
+	# add routing tables
+	for rx, ry in getSerializedCoords(width, height):
+		routerID = getRouterID(rx, ry)
+		table = {}
+		for sx, sy in getSerializedCoords(width, height):
+			sinkID = getSinkID(sx, sy)
+			sinkAddr = noc[sinkID]["address"]
+			if (rx, ry) == (sx, sy):
+				port = "local"
+			elif sx > rx:
+				port = "right"
+			elif sx < rx:
+				port = "left"
+			elif sy < ry:
+				port = "bottom"
+			else:
+				port = "top"
+			table[sinkAddr] = port
+		table["default"] = "local"
+		noc[routerID]["table"] = table
 	with open("gen/noc.json", "w") as fid:
 		fid.write(json.dumps(noc, indent = 4, sort_keys = True))
 
