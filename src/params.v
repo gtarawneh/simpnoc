1ce6140a273719bde6a60efd724ec2a3e1d77514
Ghaith Tarawneh <gtarawneh@gmail.com>
2016-11-08 18:25:12 +0000

    Remove obselete files

--- src/params.v
+++
@@ -1,7 +0,0 @@
-localparam SIZE = 8; // data bits
-localparam PORT_COUNT = 2; // number of ports
-localparam DESTINATION_BITS = 3; // number of bits to specify port
-localparam DEPTH_LOG2 = 4;
-localparam ROUTER_COUNT = 16;
-localparam SINK_COUNT = 16;
-localparam SOURCE_COUNT = 16;
