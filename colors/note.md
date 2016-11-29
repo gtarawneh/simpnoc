39c11abaf046352f3ac5d52c511b64f04447dc1b
Ghaith Tarawneh <gtarawneh@gmail.com>
2016-11-29 13:41:21 +0000

    Replace beautify script with colorpen style files

---
+++ colors/note.md
@@ -0,0 +1,6 @@
+# Note
+
+The style files in this directory are to apply coloring to simulation results
+using the tool:
+
+https://github.com/gtarawneh/colorpen
--- simulate3.sh
+++ simulate3.sh
@@ -4,4 +4,4 @@ mkdir -p {build,output}
 
 OUT="build/a.out"
 
-iverilog -I src -I src2 -o $OUT src2/arbiter_testbench.v && vvp $OUT | ./beautify.py
+iverilog -I src -I src2 -o $OUT src2/arbiter_testbench.v && vvp $OUT
