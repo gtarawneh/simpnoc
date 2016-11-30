1551f54aded4d46160dc2e0255346d35610e4f01
Ghaith Tarawneh <gtarawneh@gmail.com>
2016-11-30 14:52:36 +0000

    Add script to analyze packet transfer

---
+++ analyze.py
@@ -0,0 +1,45 @@
+#!/bin/python
+
+import sys
+import re
+import json
+
+reg1 = r"(initiated|assembled|destroyed) packet <(0x[0-9a-f]+)>"
+
+def main():
+	pat = re.compile(reg1)
+	hist = {} # packet records
+	while True:
+		line = sys.stdin.readline()
+		if line:
+			for word in pat.findall(line):
+				event = word[0] # initiated/assembled/destroyed
+				key = word[1] # packet value (hex)
+				currentRecord = hist.get(key, [])
+				currentRecord.append(event)
+				hist[key] = currentRecord
+		else:
+			break
+	packets = hist.values()
+	count = lambda isFun : len([p for p in packets if isFun(p)])
+	initiated = count(isInit)
+	correct = count(isTranCorrect)
+	ghost = count(isGhost)
+	print json.dumps(hist, indent=4)
+	print "Initiated Packets     : %d" % initiated
+	print "Transferred correctly : %d (%1.1f%%)" % (correct, 100.0*correct/initiated)
+	print "Ghost Packets         : %d" % ghost
+
+def isInit(packet):
+	return ("initiated" in packet)
+
+def isDest(packet):
+	return ("destroyed" in packet)
+
+def isTranCorrect(packet):
+	return isInit(packet) and isDest(packet)
+
+def isGhost(packet):
+	return (not isInit(packet)) and isDest(packet)
+
+main()
